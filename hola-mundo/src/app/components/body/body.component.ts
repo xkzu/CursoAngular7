2471139b702b56e16759d2d9638253266886e4db
xkzu <alvaro.vegap@gmail.com>
2018-12-06 18:05:28 -0300

    fin de la sección

---
+++ hola-mundo/src/app/components/body/body.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+
+export class BodyComponent{
+
+    frase: any = {
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parker'
+    };
+
+    personajes: string [] = ['Spiderman', 'Venom', 'Dr. Octopus'];
+}

40c836e1fd4bbce7732af246d4e0a28f4842c0cb
xkzu <alvaro.vegap@gmail.com>
2018-12-11 17:47:55 -0300

    sdf

---
+++ 8.Formularios/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { DataComponent } from './components/data/data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent,
+    DataComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 8.Formularios/src/app/components/template/template.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`
+    .ng-invalid.ng-touched:not(form) {
+      border: 1px solid red;
+    }
+  `]
+})
+export class TemplateComponent implements OnInit {
+
+  usuario:object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: "",
+    sexo: "Hombre"
+  }
+
+  paises = [{
+    codigo: "CHI",
+    nombre: "Chile"
+  },
+  {
+    codigo: "ESP",
+    nombre: "España"
+  }
+]
+
+sexos:string[] = ["Hombre", "Mujer"];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(forma:NgForm) {
+    console.log("Formulario posteado");
+    console.log("ngForm", forma);
+    console.log("valor", forma.value);
+
+    console.log("usuario", this.usuario);
+  }
+
+}
